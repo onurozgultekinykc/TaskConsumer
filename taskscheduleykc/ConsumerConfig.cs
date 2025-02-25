9ae13515b388594dbcaa27f95125aecfebaed41f
onur.ozgultekin <onur.ozgultekin@ykcgrup.com.tr>
2025-02-25 11:48:29 +0300

    sda

--- taskscheduleykc/ConsumerConfig.cs
+++ taskscheduleykc/ConsumerConfig.cs
@@ -8,6 +8,6 @@ namespace taskscheduleykc
 {
     public class ConsumerConfig
     {
-        public static string PostgresConnectionString { get; } = "Host=192.168.1.39;Port=5432;Username=postgres;Password=postgres;Database=hangfire;";
+        public static string PostgresConnectionString { get; } = "Host=c_rabbitmq;Port=5432;Username=taskconsumer;Password=soft2022;Database=hangfire;";
     }
 }
